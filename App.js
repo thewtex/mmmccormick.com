3cc0765b97a16c64286431eb52eb64d499362465
Matt McCormick <matt@mmmccormick.com>
2017-06-19 19:43:14 -0400

    Post list on homepage

--- App.js
+++ App.js
@@ -2,18 +2,12 @@ import React from 'react'
 import { Router, Route, browserHistory } from 'react-router'
 import { createApp } from '@phenomic/preset-react-app/lib/client'
 
-import BlogPostContainer from './src/BlogPostContainer.js'
-
-const Home = () =>
-  <div>
-    <p>
-    This is a homepage
-    </p>
-  </div>
+import BlogPostContainer from './src/BlogPostContainer'
+import HomeContainer from './src/HomeContainer'
 
 export default createApp(() =>
   <Router history={browserHistory}>
-    <Route path='/' component={Home} />
+    <Route path='/' component={HomeContainer} />
     <Route path='/blog/*' component={BlogPostContainer} collection='posts' />
   </Router>
 )

ed2dade2e9942a88468a7893057c90967824b657
Matt McCormick <matt@mmmccormick.com>
2017-06-19 21:01:48 -0400

    React helmet static rendering

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import { createApp } from '@phenomic/preset-react-app/lib/client'
 
 import BlogPostContainer from './src/BlogPostContainer'
 import HomeContainer from './src/HomeContainer'
+import Html from './src/Html.js'
 
-export default createApp(() =>
+const routes = () =>
   <Router history={browserHistory}>
     <Route path='/' component={HomeContainer} />
     <Route path='/after/:after' component={HomeContainer} />
     <Route path='/blog/*' component={BlogPostContainer} />
   </Router>
-)
+
+export default createApp(routes, Html)

7ee8fe45b78245efca104b2212844c59ae9b19f5
Matt McCormick <matt.mccormick@kitware.com>
2017-07-10 19:50:33 -0400

    Add flexbox to Home

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Head from 'react-helmet'
 import {
   BackgroundImage,
+  Border,
+  Box,
   Card,
+  Flex,
   Text
 } from 'rebass'
 
 import Layout from './Layout'
 import Heading from './Heading'
 import Subhead from './Subhead'
+
+const cardWidth = 270
+const boxWidth = 280
 
 const Home = ({ posts }) => (
   <Layout>
@@ -17,14 +23,22 @@ const Home = ({ posts }) => (
       <meta name='Matthew M. McCormick, Ph.D.' content='Matt McCormick&#39;s Homepage' />
     </Head>
 
-    <Card width={270} >
-      <BackgroundImage
-         src='/images/AboutHeadshot.jpg'
-	 />
-      <Subhead p={2}>
-        About
-      </Subhead>
-    </Card>
+    <Border py={4}>
+    <Flex wrap align='flex-start' justify='center'>
+
+      <Box p={2} width={boxWidth}>
+        <Card width={cardWidth} >
+          <BackgroundImage
+             src='/images/AboutHeadshot.jpg'
+             />
+          <Subhead p={2}>
+            About
+          </Subhead>
+        </Card>
+      </Box>
+
+    </Flex>
+    </Border>
 
   </Layout>
 )

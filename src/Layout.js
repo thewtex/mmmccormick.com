188a0c0aeb2485357da9a8da16943b629fae7a97
Matt McCormick <matt@mmmccormick.com>
2017-09-06 22:24:08 -0400

    Register service worker

--- src/Layout.js
+++ src/Layout.js
@@ -14,18 +14,19 @@ const Layout = ({ children }) =>
       <html lang='en' />
       <meta charSet='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <link rel="preload" as="font" href="/fonts/LinLibertine_R_subset.woff" type="font/woff" />
-      <link rel="preload" as="font" href="/fonts/LinLibertine_RB_subset.woff" type="font/woff" />
-      <link rel="preload" as="font" href="/fonts/LinLibertine_RI_subset.woff" type="font/woff" />
-      <link rel="preload" as="font" href="/fonts/LinBiolinum_R_subset.woff" type="font/woff" />
-      <link rel="stylesheet" type="text/css" href="/fonts/fonts.css" />
-      <link rel="stylesheet" href="/fonts/font-awesome-4.7.0.min.css"/>
+      <script src='/registerServiceWorker.js'></script>
+      <link rel='preload' as='font' href='/fonts/LinLibertine_R_subset.woff' type='font/woff' />
+      <link rel='preload' as='font' href='/fonts/LinLibertine_RB_subset.woff' type='font/woff' />
+      <link rel='preload' as='font' href='/fonts/LinLibertine_RI_subset.woff' type='font/woff' />
+      <link rel='preload' as='font' href='/fonts/LinBiolinum_R_subset.woff' type='font/woff' />
+      <link rel='stylesheet' type='text/css' href='/fonts/fonts.css' />
+      <link rel='stylesheet' href='/fonts/font-awesome-4.7.0.min.css' />
     </Head>
-    <NavBar/>
-      {/* Blog About Portfolio
+    <NavBar />
+    {/* Blog About Portfolio
         OpenCV Twitter GitHub LinkedIn... */}
     <div>{children}</div>
-    <Footer/>
+    <Footer />
   </Provider>
 
 export default Layout

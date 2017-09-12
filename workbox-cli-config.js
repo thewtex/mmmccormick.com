643ea994b56dc043d90666fb1f2535ef00db412a
Matt McCormick <matt@mmmccormick.com>
2017-09-11 23:42:13 -0400

    workbox pre-cache configuration

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,11 +1,16 @@
 module.exports = {
   "globDirectory": "dist/",
   "globPatterns": [
-    "**/*.{html,ico,woff,css,svg,ttf,woff2,jpg,js}"
+    "**/*.{html,ico,css,woff2,jpg,js}",
+    "fonts/*_subset.woff",
+    "images/*.svg",
+    "fonts/academicons.ttf"
   ],
   "swSrc": "src/serviceWorker.js",
   "swDest": "dist/serviceWorker.js",
   "globIgnores": [
-    "../workbox-cli-config.js"
+    "../workbox-cli-config.js",
+    "serviceWorker.js",
+    "registerServiceWorker.js"
   ]
 }

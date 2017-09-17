97f84691b7468ab93d00c1745838c1eb8f4d942f
Matt McCormick <matt@mmmccormick.com>
2017-09-16 22:14:16 -0400

    Add offline google analytics support

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,4 +1,6 @@
 importScripts('workbox-sw.prod.v2.0.1.js');
+importScripts('workbox-google-analytics.prod.v2.0.0.js');
+workbox.googleAnalytics.initialize();
 
 const workboxSW = new self.WorkboxSW({clientsClaim: true})
 workboxSW.precache([])

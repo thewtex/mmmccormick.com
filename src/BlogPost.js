1d86be681657cc4aa8557004b34401864b54d5fa
Matt McCormick <matt.mccormick@kitware.com>
2017-10-22 15:37:27 -0400

    Fix BlogPost build

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,28 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router'
 import { BodyRenderer } from '@phenomic/preset-react-app/lib/client'
 
 import Layout from './Layout'
 import PageError from './PageError'
 
-const BlogPost = ({ hasError, page }) => {
-  if (hasError) {
-    return <PageError error={page.error} />
-  }
-
-  return (
-    <Layout>
-      {isLoading && "Loading..."}
-      {!isLoading &&
-      page.node && (
-      <article>
-        <h1>{page.node.title}</h1>
-        <BodyRenderer>
-          {page.node.body}
-        </BodyRenderer>
-      </article>
-      )}
-    </Layout>
-  )
-}
+const BlogPost = ({ isLoading, page }) => (
+  <div>
+  {isLoading && "Loading..."}
+  {!isLoading &&
+  page.node && (
+    <article>
+      <h1>{page.node.title}</h1>
+      <BodyRenderer>{page.node.body}</BodyRenderer>
+    </article>
+    )}
+  <footer>
+    <Link to="/">Go to home</Link>
+  </footer>
+  </div>
+)
 
 export default BlogPost
